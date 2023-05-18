81cc8c82e52128c15bb8db4fd43f3f971eadf117
Anthony Gress <anthony.gress@envasetechnologies.com>
2023-05-18 13:11:54 -0400

    build docker script with args

---
+++ actions/scripts/build_deploy_docker.sh
@@ -0,0 +1,40 @@
+PACKAGE_JSON=./package.json
+BUILD_GRADLE=./build.gradle
+
+node_build(){
+echo  ---------- Node Variation ----------
+echo Node Version: "$(node -v)"
+APP_VERSION=$(node -pe "require('./package.json').version")
+export APP_VERSION
+echo running build for version $APP_VERSION
+}
+
+java_build(){
+    echo ---------- Java Variation ----------
+    echo Java Version: "$(java --version)"
+    chmod +x ./gradlew
+    APP_VERSION=$(./gradlew properties | grep ^version: | tr -d version: | cut -c2-)
+    export APP_VERSION
+}
+
+if [ -f "$PACKAGE_JSON" ]; then
+node_build
+elif [ -f "$BUILD_GRADLE" ]; then
+java_build
+else
+echo Unsupported language - supported languages are Java and JavaScript/Typescript
+exit 1
+fi
+
+if [ $2 ]; then
+echo ---------- Using install token ----------
+docker build -t $APP_NAME --build-arg ENVASE_CONNECT_GPR_TOKEN=$2 .
+else
+docker build -t $APP_NAME .
+fi
+
+docker tag $APP_NAME $1:$APP_VERSION
+
+echo pushing
+docker push $1 -a
+echo "image=$1:$APP_VERSION" >> $GITHUB_OUTPUT
